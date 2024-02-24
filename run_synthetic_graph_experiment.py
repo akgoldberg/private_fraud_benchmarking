2ae36feaa56654e67f534f00fc7124a4a1f8b506
Alexander Goldberg <akgoldbe@cs.cmu.edu>
2024-02-24 18:45:25 -0500

    upadtes

--- run_synthetic_graph_experiment.py
+++ run_synthetic_graph_experiment.py
@@ -186,7 +186,7 @@ def main():
 
 
     # run with privacy
-    for eps in [5.]:
+    for eps in [10.]:
         cutoff_rate = 1.
         ### NEED TO USE BEST CUTOFF RATE FOR EACH EPS, DATASET
         print('=====================================================================')
